8f4a11a29604a498acdb5024bec99cce033a58df
freda <freda@vishnus-air.fios-router.home>
2020-06-09 11:33:21 -0400

    How to convert a String to Integer without using parse condition.

---
+++ Algorithms/src/StringToInteger.java
@@ -0,0 +1,33 @@
+
+public class StringToInteger {
+
+	public int number(char ch) {
+		// 0--48, 1--49,    9--57
+		
+		return ch%48;
+	}
+	
+	public int number(String str) {
+		
+		int num=0;
+		for(int i=0; i<str.length(); i++){
+			num = num*10 + number(str.charAt(i));
+		}
+		return num;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringToInteger obj = new StringToInteger();
+		
+		String haha = "45556543";
+		int answer = obj.number(haha);
+		
+		System.out.println(answer);
+	}
+
+}
